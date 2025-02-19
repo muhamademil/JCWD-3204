c4e1f19c33a2620f0fec5077c5543723599a8f86
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-19 15:57:02 +0700

    add first next

---
+++ Module2/next-modules/first-next-app/src/pages/index.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react'
+import styles from './index.module.css'
+import TestComponent, { IPerson } from '@/components/Test/TestComponent'
+
+export default function Home() {
+  const [steps, setStep] = useState<number>(0)
+  const [isOn, setIsOn] = useState<boolean>(false)
+
+  // simulasi membuat undian
+  const [number, setNumber] = useState<number>(0)
+  const [prize, setPrize] = useState<string>("")
+
+  function drawNumber() {
+    // bikin undian
+    const randomNumber = (Math.floor(Math.random() * 50))
+    setNumber(randomNumber)
+
+    // bikin rewardnya
+    let reward;
+    if (randomNumber >= 1 && randomNumber <= 10) {
+      reward = "T-Shirt"
+    } else if (randomNumber >= 11 && randomNumber <= 20) {
+      reward = "Mug"
+    } else if (randomNumber >= 21 && randomNumber <= 30) {
+      reward = "Voucher Rp50.000"
+    } else if (randomNumber >= 31 && randomNumber <= 40) {
+      reward = "Smartphone"
+    } else {
+      reward = "No prize, try again!"
+    }
+
+    // simpan reward ke setPrize
+    setPrize(reward)
+  }
+
+
+  function addStep() {
+    setStep(steps + 1)
+  }
+
+  function removeStep() {
+    if (steps <= 0) {
+      return
+    } else {
+      setStep(steps - 1)
+    }
+  }
+
+  function triggerLamp() {
+    if (!isOn) {
+      setIsOn(true)
+    } else {
+      setIsOn(false)
+    }
+  }
+
+  const user: IPerson[] = [
+    {
+      name: "Agus",
+      age: 23
+    },
+    {
+      name: "Fajri",
+      age: 10
+    },
+    {
+      name: "Azmi",
+      age: 17
+    },
+    {
+      name: "Emil",
+      age: 30
+    }
+  ]
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.subContainer}>
+        <h3> --- Contoh state untuk menghitung steps --- </h3>
+        <h1>Step : {steps}</h1>
+        <div className={styles.containerButton}>
+          <button className={styles.stepButton} onClick={addStep}> Add Step</button>
+          <button className={styles.stepButton} onClick={removeStep}> Remove Step</button>
+        </div>
+      </div>
+      <div className={styles.subContainer}>
+        <h3> --- Contoh state untuk mematikan dan menyalakan lampu --- </h3>
+        <h3>Keadaan Lampu : {isOn ? 'Lampu Menyala' : 'Lampu Mati'}</h3>
+        <button className={styles.lampButton} onClick={triggerLamp}>Click</button>
+      </div>
+      <div className={styles.subContainer}>
+        <h3> --- Contoh state untuk undian --- </h3>
+        <h3>Kamu dapat nomor undian : {number}</h3>
+        <h4>Hadiah yang kamu dapat kan adalah : {prize}</h4>
+        <button className={styles.drawButton} onClick={drawNumber}>Click</button>
+      </div>
+      <div className={styles.subContainer}>
+        <h3> --- Contoh props untuk Test Component --- </h3>
+        {
+          user.map((item: IPerson) => {
+            return (
+              <TestComponent
+                name={item.name}
+                age={item.age}
+              />
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
