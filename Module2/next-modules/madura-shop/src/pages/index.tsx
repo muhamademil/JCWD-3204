3f0aa655531f8b257532a9daec1ee85f930616ff
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-20 15:12:46 +0700

    add loading component

--- Module2/next-modules/madura-shop/src/pages/index.tsx
+++ Module2/next-modules/madura-shop/src/pages/index.tsx
@@ -6,11 +6,14 @@ export default function LandingPage() {
     <Layout>
       <div className='flex w-full h-full'>
         <div className='w-full h-screen flex flex-col gap-y-20 justify-center items-start bg-amber-400 p-10'>
-          <div className='font-semibold'>
+          <div className='font-bold text-2xl'>
+            <p>Madura Shop - Buka Sampai Kiamat</p>
+          </div>
+          <div className='font-medium'>
             <p>Belanja Sembako Murah & Terjangkau!</p>
             <p>Dapatkan harga terbaik untuk kebutuhan sehari-hari hanya di Madura Shop</p>
           </div>
-          <button className='bg-amber-600 hover:bg-amber-700 rounded-md text-center p-3'>Belanja Sekarang</button>
+          <button className='bg-amber-600 hover:bg-amber-700 rounded-md text-center p-3 font-semibold'>Belanja Sekarang</button>
         </div>
         <div className='w-full h-screen flex justify-center items-center'>
           <img src='https://i.pinimg.com/736x/69/9e/63/699e632ee8a0e8eb0be76f42bd7adc55.jpg'
