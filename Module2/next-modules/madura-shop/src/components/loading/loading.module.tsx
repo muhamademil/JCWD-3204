9c35abef72033c3498eac50a03b54e3370fce1fb
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-20 15:58:02 +0700

    add login

--- Module2/next-modules/madura-shop/src/components/loading/loading.module.tsx
+++ Module2/next-modules/madura-shop/src/components/loading/loading.module.tsx
@@ -3,7 +3,7 @@ export default function Loading() {
         <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
             <div className="flex flex-col items-center">
                 <div className="w-16 h-16 border-4 border-amber-600 border-t-transparent rounded-full animate-spin">
-                    <p className="mt-4 text-lg font-semibold text-black">Loading ...</p>
+                    <p className="mt-4 text-sm font-semibold text-black">Loading</p>
                 </div>
             </div>
         </div>
