7ed235fccb74099993c39595a5e3aca9a885e1fc
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-21 10:26:45 +0700

    add product page

--- Module2/next-modules/madura-shop/src/pages/index.tsx
+++ Module2/next-modules/madura-shop/src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import Layout from '@/components/layout/layout.module'
 
 export default function LandingPage() {
+
+  const router = useRouter()
+
   return (
     <Layout>
       <div className='flex w-full h-full'>
@@ -13,7 +17,10 @@ export default function LandingPage() {
             <p>Belanja Sembako Murah & Terjangkau!</p>
             <p>Dapatkan harga terbaik untuk kebutuhan sehari-hari hanya di Madura Shop</p>
           </div>
-          <button className='bg-amber-600 hover:bg-amber-700 rounded-md text-center p-3 font-semibold'>Belanja Sekarang</button>
+          <button
+            className='bg-amber-600 hover:bg-amber-700 rounded-md text-center p-3 font-semibold'
+            onClick={() => router.push('/auth')}
+          >Belanja Sekarang</button>
         </div>
         <div className='w-full h-screen flex justify-center items-center'>
           <img src='https://i.pinimg.com/736x/69/9e/63/699e632ee8a0e8eb0be76f42bd7adc55.jpg'
--- Module2/next-modules/madura-shop/src/pages/product/index.tsx
+++ Module2/next-modules/madura-shop/src/pages/product/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import Layout from '@/components/layout/layout.module'
+import Card from '@/components/card/card.module'
+
+import { data, DataProduct } from '@/dummy/data'
 
 export default function Product() {
     return (
-        <div>Product</div>
+        <Layout>
+            <div className='grid grid-cols-3 gap-5 p-5'>
+                {
+                    data.map((item: DataProduct) => {
+                        return (
+                            <Card
+                                id={item.id}
+                                title={item.title}
+                                description={item.description}
+                                image={item.image}
+                                price={item.price}
+                                category={item.category}
+                            />
+                        )
+                    })
+                }
+            </div>
+        </Layout>
     )
 }
