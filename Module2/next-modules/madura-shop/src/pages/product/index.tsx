1e71ee3e09fa9e66f0135b7f5010ae06cae15cb1
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-21 11:27:44 +0700

    add detail product

--- Module2/next-modules/madura-shop/src/pages/product/index.tsx
+++ Module2/next-modules/madura-shop/src/pages/product/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import Layout from '@/components/layout/layout.module'
 import Card from '@/components/card/card.module'
 
 import { data, DataProduct } from '@/dummy/data'
 
 export default function Product() {
+
+    const router = useRouter()
+
     return (
         <Layout>
             <div className='grid grid-cols-3 gap-5 p-5'>
@@ -18,6 +22,7 @@ export default function Product() {
                                 image={item.image}
                                 price={item.price}
                                 category={item.category}
+                                onClick={() => router.push(`product/${item.id}`)}
                             />
                         )
                     })
