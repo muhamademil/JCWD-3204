83619d3a72dab847939930dcf5689357116b6652
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-21 15:08:25 +0700

    add search, category, sort by

--- Module2/next-modules/madura-shop/src/pages/product/index.tsx
+++ Module2/next-modules/madura-shop/src/pages/product/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import Layout from '@/components/layout/layout.module'
 import Card from '@/components/card/card.module'
@@ -7,13 +7,68 @@ import { data, DataProduct } from '@/dummy/data'
 
 export default function Product() {
 
+    const [search, setSearch] = useState<string>("")
+    const [category, setCategory] = useState<string>("All")
+    const [sort, setSort] = useState<string>("None")
     const router = useRouter()
+
+    const categories = ['All', 'Beras', 'Minyak', 'Gula', 'Telur']
+    const sortOptions = ['None', 'Harga Terendah ke Harga Tertinggi', 'Harga Tertinggi ke Terendah']
+
+    const filteredProducts = data
+        .filter((product) =>
+            category === "All" ? true : product.category === category
+        )
+        .filter((product) =>
+            [product.title, product.description, product.price.toString()].some((field) =>
+                field.toLowerCase().includes(search.toLowerCase())
+            )
+        )
+        .sort((low, high) => {
+            if (sort === "Harga Terendah ke Harga Tertinggi") {
+                return low.price - high.price
+            } else if (sort === "Harga Tertinggi ke Terendah") {
+                return high.price - low.price
+            } else {
+                return 0
+            }
+        })
 
     return (
         <Layout>
+            <div className='w-full h-full flex gap-x-5 p-5 bg-red-500 text-black'>
+                <input
+                    type="text"
+                    placeholder='Search ...'
+                    onChange={(e) => setSearch(e.target.value)}
+                    className='border p-2 rounded-md w-1/2'
+                />
+                <select
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    className='border p-2 rounded-md'
+                >
+                    {
+                        categories.map((item) => (
+                            <option>{item}</option>
+                        ))
+                    }
+                </select>
+                <select
+                    value={sort}
+                    onChange={(e) => setSort(e.target.value)}
+                    className='border p-2 rounded-md'
+                >
+                    {
+                        sortOptions.map((item) => (
+                            <option>{item}</option>
+                        ))
+                    }
+                </select>
+            </div>
             <div className='grid grid-cols-3 gap-5 p-5'>
                 {
-                    data.map((item: DataProduct) => {
+                    filteredProducts.map((item: DataProduct) => {
                         return (
                             <Card
                                 id={item.id}
