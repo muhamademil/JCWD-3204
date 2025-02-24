df3294059655c183d0dd70c9ea53417ee90754cb
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-24 15:54:31 +0700

    add useref

--- Module2/next-modules/todolist-app/src/app/hooks/ref/page.tsx
+++ Module2/next-modules/todolist-app/src/app/hooks/ref/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import CountLayout from '@/components/molecules/count-layout/count-layout.module'
+import Layout from '@/components/molecules/layout/layout.module'
 
 export default function Ref() {
     return (
-        <div>Ref Example Page</div>
+        <Layout>
+            <CountLayout />
+        </Layout>
     )
 }
--- Module2/next-modules/todolist-app/src/components/molecules/count-layout/count-layout.module.tsx
+++ Module2/next-modules/todolist-app/src/components/molecules/count-layout/count-layout.module.tsx
@@ -1,35 +1,69 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useCount } from '@/custom-hooks/useCount'
 import Button from '@/components/atomics/button/button.module'
 
 export default function CountLayout() {
 
     const { count, addCount, decreaseCount } = useCount()
+    const [renderCount, setRenderCount] = useState(0)
+    const countRef = useRef(0)
 
-    useEffect(() => {
-        console.log('mounting component CountLayout ...') // -> fase ketika komponen mengalami mounting
-        // console.log('updating count : ', count) // -> fase ketika komponen mengalami updating
+    // useEffect(() => {
+    //     console.log('mounting component CountLayout ...') // -> fase ketika komponen mengalami mounting
+    //     // console.log('updating count : ', count) // -> fase ketika komponen mengalami updating
 
-        return () => {
-            console.log('unmounting component CountLayout ...') // -> fase ketika komponen mengalami unmounting
-        }
-    }, [])
+    //     return () => {
+    //         console.log('unmounting component CountLayout ...') // -> fase ketika komponen mengalami unmounting
+    //     }
+    // }, [])
+
+    console.log('state -> ', count)
+
+    function increaseCount() {
+        countRef.current += 1;
+    }
+
+    function removeCount() {
+        countRef.current -= 1;
+    }
+
+    console.log("count ref -> ", countRef.current)
 
 
     return (
-        <div className='w-full h-full bg-slate-200 flex justify-center items-center'>
-            <div className='flex w-80 gap-x-5'>
+        <div className='w-full h-full bg-slate-200 flex flex-col justify-center items-center'>
+            <h2 className='font-semibold text-lg text-center text-black my-5'> ---- Count with useState ---- </h2>
+            <div className='flex w-full gap-x-5 bg-slate-700 p-5 rounded-md'>
                 <Button
-                    id='add-count'
-                    title='Add Count'
+                    id='add-count-state'
+                    title='Add Count with useState'
                     onClick={addCount}
                 />
-                <p className='font-semibold text-black mx-5'>Count : {count}</p>
+                <p className='font-semibold text-black mx-5'>Count with useState : {count}</p>
                 <Button
-                    id='decrease-count'
-                    title='Decrease Count'
+                    id='decrease-count-state'
+                    title='Decrease Count with useState'
                     onClick={decreaseCount}
+                />
+            </div>
+            <h2 className='font-semibold text-lg text-center text-black my-5'> ---- Count with useRef --- </h2>
+            <div className='flex w-full gap-x-5 bg-slate-700 p-5 rounded-md'>
+                <Button
+                    id='add-count-ref'
+                    title='Add Count with useRef'
+                    onClick={increaseCount}
+                />
+                <Button
+                    id='re-render-countref'
+                    title='Re-render countRef'
+                    onClick={() => setRenderCount(renderCount + 1)}
+                />
+                <p className='font-semibold text-black mx-5'>Count with useRef : {countRef.current}</p>
+                <Button
+                    id='decrease-count-ref'
+                    title='Decrease Count with useRef'
+                    onClick={removeCount}
                 />
             </div>
         </div>
