7fc2ea71e6781655e0953c0a2f26d890db1f98c1
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-26 13:33:04 +0700

    add card

---
+++ Module2/next-modules/sumber-makmur-app/src/components/atomics/card.module.tsx
@@ -0,0 +1,21 @@
+export interface ICard {
+    name: string;
+    category: string;
+    imageUrl: string;
+    description: string;
+    price: number;
+}
+
+export default function Card({ name, category, imageUrl, description, price }: ICard) {
+    return (
+        <div className="bg-white shadow-md rounded-2xl overflow-hidden w-80 h-full border border-gray-200">
+            <img src={imageUrl === "https://www.sumbermakmur.com" ? imageUrl : 'https://i.pinimg.com/736x/2a/86/a5/2a86a560f0559704310d98fc32bd3d32.jpg'} alt={name} width={320} height={200} className="w-full h-48 object-cover" />
+            <div className="p-4">
+                <h2 className="text-lg font-semibold text-gray-900">{name}</h2>
+                <p className="text-sm text-gray-500">{category}</p>
+                <p className="text-gray-700 mt-2 text-sm">{description}</p>
+                <p className="text-lg font-bold text-blue-600 mt-2">Rp {price.toLocaleString()}</p>
+            </div>
+        </div>
+    )
+}
--- Module2/next-modules/sumber-makmur-app/src/components/molecules/list-product.module.tsx
+++ Module2/next-modules/sumber-makmur-app/src/components/molecules/list-product.module.tsx
@@ -1,13 +1,16 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { axiosInstance } from '@/utils/products.api'
+import Card, { ICard } from '@/components/atomics/card.module'
 
 export default function ListProduct() {
+
+    const [product, setProduct] = useState<[]>([])
 
     async function getAllProducts() {
         try {
             const response = await axiosInstance.get('/products')
-            console.log(response.data)
+            setProduct(response.data)
         } catch (error) {
             console.log(error)
         }
@@ -19,6 +22,21 @@ export default function ListProduct() {
 
 
     return (
-        <div>ListProduct</div>
+        <div className='grid grid-cols-3 justify-center items-center w-full h-full p-3'>
+            {
+                product?.map((item: ICard, key: number) => {
+                    return (
+                        <Card
+                            key={key}
+                            name={item?.name}
+                            category={item?.category}
+                            description={item?.description}
+                            imageUrl={item?.imageUrl}
+                            price={item?.price}
+                        />
+                    )
+                })
+            }
+        </div>
     )
 }
