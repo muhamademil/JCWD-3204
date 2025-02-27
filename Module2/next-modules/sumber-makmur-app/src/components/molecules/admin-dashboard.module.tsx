11d69ebeb830726226e9041d2c934b53e5facee8
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-27 14:51:30 +0700

    add crud admin dashboard

--- Module2/next-modules/sumber-makmur-app/src/components/molecules/admin-dashboard.module.tsx
+++ Module2/next-modules/sumber-makmur-app/src/components/molecules/admin-dashboard.module.tsx
@@ -4,92 +4,76 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { productSchema, ProductInput } from '@/utils/validator/product.schema'
 import { axiosInstance } from '@/utils/api/products.api'
-import * as Dialog from '@radix-ui/react-dialog'
+import Swal from 'sweetalert2'
 
 export default function AdminDashboard() {
-
-    const [products, setProduct] = useState<ProductInput[] | null>(null)
+    const [products, setProducts] = useState<ProductInput[] | null>(null)
     const [editProduct, setEditProduct] = useState<ProductInput | null>(null)
-    const { register, handleSubmit, formState: { errors } } = useForm<ProductInput>({
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const { register, handleSubmit, formState: { errors }, setValue, reset } = useForm<ProductInput>({
         resolver: zodResolver(productSchema)
     })
 
     async function fetchProducts() {
         const response = await axiosInstance.get('/products')
-        setProduct(response.data)
+        setProducts(response.data)
     }
 
-    function onSubmitProduct() { }
-    function deleteProduct() { }
+    async function onSubmitProduct(data: ProductInput) {
+        const bodyRequest: ProductInput = {
+            name: data.name,
+            price: Number(data.price),
+            stock: Number(data.stock)
+        }
+        if (editProduct) {
+            await axiosInstance.put(`/products/${editProduct.objectId}`, bodyRequest)
+            Swal.fire('Success', 'Produk berhasil diperbarui', 'success')
+        } else {
+            await axiosInstance.post('/products', bodyRequest)
+            Swal.fire('Success', 'Produk berhasil dibuat', 'success')
+        }
+
+        fetchProducts()
+        setEditProduct(null)
+        reset()
+        setIsModalOpen(false)
+    }
+
+    async function deleteProduct(id: string | undefined) {
+        try {
+            await axiosInstance.delete(`/products/${id}`)
+            Swal.fire('Success', 'Produk berhasil dihapus', 'success')
+            fetchProducts()
+        } catch {
+            Swal.fire('Failed', 'Produk gagal dihapus. Coba lagi', 'error')
+        }
+    }
 
     useEffect(() => {
         fetchProducts()
     }, [])
 
-    function ProductForm() {
-        return (
-            <Dialog.Root>
-                {/* Button untuk tambah produk */}
-                <Dialog.Trigger className='px-4 py-2 bg-blue-600 text-white rounded'>
-                    Tambah Produk
-                </Dialog.Trigger>
-
-                {/* Form untuk tambah/edit */}
-                <Dialog.Portal>
-                    <Dialog.Overlay className='fixed inset-0 bg-black opacity-30'>
-                        <Dialog.Content
-                            className='fixed bg-white p-6 rounded shadow-lg top-1/2 left-1/2 
-                        transform-translate-x-1/2 -translate-y-1/2 w-96'>
-                            <h2 className='text-xl font-bold mb-4'>{editProduct ? 'Edit Produk' : 'Tambah Produk'}</h2>
-                            <form action="">
-                                <div className='mt-4'>
-                                    <label>Nama Produk</label>
-                                    <input
-                                        type='text'
-                                        className='block w-full p-2 border rounded'
-                                        {...register('name')}
-                                    />
-                                    {
-                                        errors.name &&
-                                        <p className='text-red-500 mt-2'>{errors.name.message}</p>
-                                    }
-                                </div>
-                                <div className='mt-4'>
-                                    <label>Harga</label>
-                                    <input
-                                        type='number'
-                                        className='block w-full p-2 border rounded'
-                                        {...register('price')}
-                                    />
-                                    {
-                                        errors.price &&
-                                        <p className='text-red-500 mt-2'>{errors.price.message}</p>
-                                    }
-                                </div>
-                                <div className='mt-4'>
-                                    <label>Stock</label>
-                                    <input
-                                        type='number'
-                                        className='block w-full p-2 border rounded'
-                                        {...register('stock')}
-                                    />
-                                    {
-                                        errors.stock &&
-                                        <p className='text-red-500 mt-2'>{errors.stock.message}</p>
-                                    }
-                                </div>
-                            </form>
-                        </Dialog.Content>
-                    </Dialog.Overlay>
-                </Dialog.Portal>
-            </Dialog.Root>
-        )
+    function openModal(product?: ProductInput) {
+        if (product) {
+            setEditProduct(product)
+            setValue('name', product.name)
+            setValue('price', product.price)
+            setValue('stock', product.stock)
+        } else {
+            setEditProduct(null)
+            reset()
+        }
+        setIsModalOpen(true)
     }
 
     return (
-        <div className='w-screen min-h-screen p-6 bg-gray-100'>
+        <div className='w-screen min-h-screen p-6 bg-gray-100 mt-36'>
             <h1 className='text-2xl font-bold mb-4'>Sumber Makmur Dashboard</h1>
-            <ProductForm/>
+            <button onClick={() => openModal()} className='px-4 py-2 bg-blue-600 text-white rounded'>
+                Tambah Produk
+            </button>
+
             <table className='w-full mt-6 border'>
                 <thead>
                     <tr className='bg-gray-200 border'>
@@ -100,29 +84,54 @@ export default function AdminDashboard() {
                     </tr>
                 </thead>
                 <tbody>
-                    {
-                        products?.map((item: ProductInput, key: number) => {
-                            return (
-                                <tr key={key}>
-                                    <td className='p-2 border'>{item.name}</td>
-                                    <td className='p-2 border'>{item.price}</td>
-                                    <td className='p-2 border'>{item.stock}</td>
-                                    <td className='p-2 border flex gap-2'>
-                                        <Dialog.Root>
-                                            <Dialog.Trigger className='px-2 py-1 bg-yellow-500 text-white rounded'>
-                                                Edit
-                                            </Dialog.Trigger>
-                                        </Dialog.Root>
-                                        <button
-                                            className='px-2 py-1 bg-red-500 text-white rounded'
-                                        >Hapus</button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
+                    {products?.map((item, key) => (
+                        <tr key={key}>
+                            <td className='p-2 border'>{item.name}</td>
+                            <td className='p-2 border'>{item.price}</td>
+                            <td className='p-2 border'>{item.stock}</td>
+                            <td className='p-2 border flex gap-2'>
+                                <button onClick={() => openModal(item)} className='px-2 py-1 bg-yellow-500 text-white rounded'>
+                                    Edit
+                                </button>
+                                <button onClick={() => deleteProduct(item?.objectId)} className='px-2 py-1 bg-red-500 text-white rounded'>
+                                    Hapus
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
+
+            {isModalOpen && (
+                <div className='fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center'>
+                    <div className='bg-white p-6 rounded shadow-lg w-96'>
+                        <h2 className='text-lg font-bold'>{editProduct ? 'Edit Produk' : 'Tambah Produk'}</h2>
+                        <form onSubmit={handleSubmit(onSubmitProduct)}>
+                            <div className='mt-4'>
+                                <label>Nama Produk</label>
+                                <input type='text' className='block w-full p-2 border rounded' {...register('name')} />
+                                {errors.name && <p className='text-red-500 mt-2'>{errors.name.message}</p>}
+                            </div>
+                            <div className='mt-4'>
+                                <label>Harga</label>
+                                <input type='number' className='block w-full p-2 border rounded'   {...register('price', { setValueAs: v => Number(v) })} />
+                                {errors.price && <p className='text-red-500 mt-2'>{errors.price.message}</p>}
+                            </div>
+                            <div className='mt-4'>
+                                <label>Stock</label>
+                                <input type='number' className='block w-full p-2 border rounded'   {...register('stock', { setValueAs: v => Number(v) })} />
+                                {errors.stock && <p className='text-red-500 mt-2'>{errors.stock.message}</p>}
+                            </div>
+                            <button type='submit' className='w-full mt-6 p-2 bg-blue-600 text-white rounded'>
+                                {editProduct ? 'Update' : 'Tambah'}
+                            </button>
+                            <button onClick={() => setIsModalOpen(false)} className='w-full mt-2 p-2 bg-gray-400 text-white rounded'>
+                                Tutup
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
--- Module2/next-modules/sumber-makmur-app/src/utils/validator/product.schema.ts
+++ Module2/next-modules/sumber-makmur-app/src/utils/validator/product.schema.ts
@@ -1,9 +1,10 @@
 import { z as zod } from 'zod'
 
 export const productSchema = zod.object({
+    objectId: zod.string().optional(),
     name: zod.string().min(3, 'Nama produk minimal 3 karakter'),
     price: zod.number().min(1000, 'Harga minimal Rp1.000'),
-    stock: zod.number().min(1, 'Stok minimal 1')
+    stock: zod.number().min(1, 'Stok minimal 1'),
 })
 
 export type ProductInput = zod.infer<typeof productSchema>
