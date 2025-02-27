6358887a2517f83506ca98ef8c6c780aeb141235
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-02-27 10:41:29 +0700

    create validator for product input

---
+++ Module2/next-modules/sumber-makmur-app/src/utils/validator/product.schema.ts
@@ -0,0 +1,9 @@
+import { z as zod } from 'zod'
+
+export const productSchema = zod.object({
+    name: zod.string().min(3, 'Nama produk minimal 3 karakter'),
+    price: zod.number().min(1000, 'Harga minimal Rp1.000'),
+    stock: zod.number().min(1, 'Stok minimal 1')
+})
+
+export type ProductInput = zod.infer<typeof productSchema>
